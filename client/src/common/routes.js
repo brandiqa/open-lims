116244f7106078fb0ae83bf2f7a2ff40fc708e89
Michael Wanyoike <micwanyoike@gmail.com>
2017-05-27 21:07:18 +0300

    partially implemented sales page

--- client/src/common/routes.js
+++ client/src/common/routes.js
@@ -1,11 +1,13 @@
 const app = '/app/';
 const dashboard = '/dashboard/';
+const sales = dashboard + 'sales';
 
 export const routes = {
   app,
   dashboard,
   home: app,
-  sales: dashboard + 'sales',
+  sales,
+  customer: sales + '/customer',
   inventory: dashboard + 'inventory',
   lab: dashboard + 'lab',
   admin: dashboard + 'admin'
--- client/src/sales/sales-page.js
+++ client/src/sales/sales-page.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
+import { Grid, Menu } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
+import { routes } from '../common/routes';
 
 class SalesPage extends Component {
   render() {
     return (
-      <p>Sales Dashboard under construction</p>
+      <Grid stackable>
+        <Grid.Column width={3} style={{paddingRight:0}}>
+          <Menu vertical>
+            <NavLink className='item grey' activeClassName='active' exact to={routes.sales}>
+              Sales
+            </NavLink>
+            <NavLink className='item grey' activeClassName='active' to={routes.customer}>
+              Customers
+            </NavLink>
+            <NavLink className='item grey' activeClassName='active' exact to={routes.sales}>
+              Invoice
+            </NavLink>
+            <NavLink className='item grey' activeClassName='active' exact to={routes.sales}>
+              Payments
+            </NavLink>
+            <NavLink className='item grey' activeClassName='active' exact to={routes.sales}>
+              Accounts
+            </NavLink>
+          </Menu>
+        </Grid.Column>
+        <Grid.Column width={3} style={{paddingRight:0}}>
+          put page content here
+        </Grid.Column>
+      </Grid>
     )
   }
 }
