ddf75979659daf7db6e3cfb8172b933b49de3746
Michael Wanyoike <micwanyoike@gmail.com>
2017-05-27 22:31:36 +0300

    updated and added new routes

--- client/src/common/routes.js
+++ client/src/common/routes.js
@@ -1,17 +1,28 @@
 const app = '/app/';
 const dashboard = '/dashboard/';
 const sales = dashboard + 'sales';
+const inventory = dashboard + 'inventory';
+const lab = dashboard + 'lab';
+const admin = dashboard + 'admin';
 
 export const routes = {
   app,
   dashboard,
   home: app,
   sales,
-  customer: sales + '/customer',
-  invoice: sales + '/invoice',
-  payment: sales + '/payment',
-  account: sales + '/accounts',
-  inventory: dashboard + 'inventory',
-  lab: dashboard + 'lab',
-  admin: dashboard + 'admin'
+  customers: sales + '/customers',
+  invoices: sales + '/invoices',
+  payments: sales + '/payments',
+  accounts: sales + '/accounts',
+  inventory,
+  products: inventory + '/products',
+  stock: inventory + '/stock',
+  lab,
+  services: lab + '/services',
+  tasks: lab + '/tasks',
+  jobs: lab + '/jobs',
+  jobTasks: lab + '/jobtasks'
+  admin,
+  users: admin + '/users',
+  roles: admin + '/roles',
 }
--- client/src/sales/sales-page.js
+++ client/src/sales/sales-page.js
@@ -17,16 +17,16 @@ class SalesPage extends Component {
             <NavLink className='item orange' activeClassName='active' exact to={routes.sales}>
               Sales
             </NavLink>
-            <NavLink className='item orange' activeClassName='active' to={routes.customer}>
+            <NavLink className='item orange' activeClassName='active' to={routes.customers}>
               Customers
             </NavLink>
-            <NavLink className='item orange' activeClassName='active' exact to={routes.invoice}>
+            <NavLink className='item orange' activeClassName='active' exact to={routes.invoices}>
               Invoice
             </NavLink>
-            <NavLink className='item orange' activeClassName='active' exact to={routes.payment}>
+            <NavLink className='item orange' activeClassName='active' exact to={routes.payments}>
               Payments
             </NavLink>
-            <NavLink className='item orange' activeClassName='active' exact to={routes.account}>
+            <NavLink className='item orange' activeClassName='active' exact to={routes.accounts}>
               Accounts
             </NavLink>
           </Menu>
@@ -34,9 +34,9 @@ class SalesPage extends Component {
         <Grid.Column width={13} style={{paddingRight:0}}>
             <Route component={SalesDashboard} exact path={routes.sales} />
             <Route component={Customers} exact path={routes.customer} />
-            <Route component={Invoices} exact path={routes.invoice} />
-            <Route component={Payments} exact path={routes.payment} />
-            <Route component={Accounts} exact path={routes.account} />
+            <Route component={Invoices} exact path={routes.invoices} />
+            <Route component={Payments} exact path={routes.payments} />
+            <Route component={Accounts} exact path={routes.accounts} />
         </Grid.Column>
       </Grid>
     );
