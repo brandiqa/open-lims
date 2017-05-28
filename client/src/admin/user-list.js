55cf7ec654771fe10cf7eb142dccd55e1c3089c6
Michael Wanyoike <micwanyoike@gmail.com>
2017-05-28 19:38:57 +0300

    Partially implemented User list

--- client/src/admin/user-list.js
+++ client/src/admin/user-list.js
@@ -1,13 +1,49 @@
 import React from 'react';
-// import stores from './stores'
+import { observer } from 'mobx-react';
+import { Message, Icon, Card } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
+@observer
 class UserList extends React.Component {
+
+  componentDidMount() {
+    this.props.store.fetchAll();
+  }
+
   render() {
-    const { store } = this.props;
+    const { entities, loading, errors, deleteOne } = this.props.store;
+    const messages = errors.messages ? errors.messages.toJS() : [];
+
+    const errorMessages = (
+      <Message negative header={errors.global} list={messages.reverse()}/>
+    )
+
+    const fetchingMessage = (
+      <Message icon info>
+        <Icon name='circle notched' loading />
+        <Message.Content>
+           <Message.Header>Just one moment</Message.Header>
+           We are fetching that content for you.
+       </Message.Content>
+      </Message>
+    )
+
+    const emptyMessage = (
+      <Message icon info>
+        <Icon name='warning circle' />
+        <Message.Content>
+           <Message.Header>No User(s) Found</Message.Header>
+           <span>Add some user(s) to get started..</span>
+          <Link to={'/contacts/new'} className="ui button primary right floated">Add New User</Link>
+       </Message.Content>
+      </Message>
+    )
 
     return (
       <div>
-        <p>User List</p>
+        { loading && fetchingMessage }
+        { entities.length === 0 && !loading  && !errors.global && emptyMessage }
+        { errors.global && errorMessages}
       </div>
     )
   }
