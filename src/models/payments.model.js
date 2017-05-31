59fe68cf1a56b049a4cf3dc297d25bd9ca3ca5a2
Michael Wanyoike <micwanyoike@gmail.com>
2017-05-31 20:23:22 +0300

    defined payments schema

--- src/models/payments.model.js
+++ src/models/payments.model.js
@@ -1,11 +1,12 @@
 // payments-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const payments = new mongooseClient.Schema({
-    text: { type: String, required: true },
+    invoice: { type: mongooseClient.Schema.Types.ObjectId, ref: 'invoices' },
+    amount: { type: Number, required: true },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
