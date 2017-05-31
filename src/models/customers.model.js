6dacf16c88f291bfd327bc1e5be064e7bd57e87f
Michael Wanyoike <micwanyoike@gmail.com>
2017-05-31 20:47:22 +0300

    defined customers schema

--- src/models/customers.model.js
+++ src/models/customers.model.js
@@ -1,11 +1,18 @@
 // customers-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
+require('mongoose-type-email');
+
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const customers = new mongooseClient.Schema({
-    text: { type: String, required: true },
+    name: { type: String, required: true },
+    phone: { type: String, required:false },
+    email: { type: mongooseClient.SchemaTypes.Email, required: true, unique: true },
+    address: { type: String, required:true },
+    creditLimit: { type: Number, required:true, default:0 },
+    invoices: [{ type: mongooseClient.Schema.Types.ObjectId, ref: 'invoices' }],
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
