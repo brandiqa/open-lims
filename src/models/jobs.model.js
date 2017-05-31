eebc588fece5f5a59d1c9bc10002b7f7d860e672
Michael Wanyoike <micwanyoike@gmail.com>
2017-05-31 20:51:46 +0300

    defined schema for jobs

--- src/models/jobs.model.js
+++ src/models/jobs.model.js
@@ -1,13 +1,16 @@
 // jobs-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const jobs = new mongooseClient.Schema({
-    text: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now }
+    invoice: { type: mongooseClient.Schema.Types.ObjectId, ref: 'invoices' },
+    service: { type: mongooseClient.Schema.Types.ObjectId, ref: 'services' },
+    notes: { type: String, required: true },
+    expected: { type: Number, required: true },
+    startDate: { type: Date, default: Date.now },
+    finishDate: { type: Date, default: Date.now }
   });
 
   return mongooseClient.model('jobs', jobs);
