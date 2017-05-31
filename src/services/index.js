305aac07dde9b0a8c134fb032249561dce431748
Michael Wanyoike <micwanyoike@gmail.com>
2017-05-31 19:50:42 +0300

    generated customers service

--- src/services/index.js
+++ src/services/index.js
@@ -4,6 +4,7 @@ const products = require('./products/products.service.js');
 const tasks = require('./tasks/tasks.service.js');
 const services = require('./services/services.service.js');
 const invoices = require('./invoices/invoices.service.js');
+const customers = require('./customers/customers.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(users);
@@ -12,4 +13,5 @@ module.exports = function () {
   app.configure(tasks);
   app.configure(services);
   app.configure(invoices);
+  app.configure(customers);
 };
