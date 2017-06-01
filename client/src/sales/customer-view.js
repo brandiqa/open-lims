db78d8d9db0f681ace1a6dbdb9ce621ddd797db8
Michael Wanyoike <micwanyoike@gmail.com>
2017-06-01 22:28:43 +0300

    implemented basic grid in customer-view

--- client/src/sales/customer-view.js
+++ client/src/sales/customer-view.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { observer } from 'mobx-react';
+import { Grid, Segment, Menu }  from 'semantic-ui-react';
+// import { brand } from '../config/theme';
 
 @observer
 class CustomerView extends React.Component {
+
   render() {
+    const grid = (
+      <Grid stackable>
+          <Grid.Column width={6}>
+            Customer Detail
+          </Grid.Column>
+          <Grid.Column width={10}>
+            Customer Invoices
+          </Grid.Column>
+      </Grid>
+    );
+
     return(
       <div>
-        Customer View
+        {grid}
       </div>
     )
   }
