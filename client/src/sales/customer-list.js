b1db5d6f69b10e7b1435dbc54005e14733dec64f
Michael Wanyoike <micwanyoike@gmail.com>
2017-06-01 22:37:48 +0300

    Implemented menus in customer-view

--- client/src/sales/customer-list.js
+++ client/src/sales/customer-list.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { observer } from 'mobx-react';
 import DomainList from '../templates/domain-list';
 
--- client/src/sales/customer-view.js
+++ client/src/sales/customer-view.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import { observer } from 'mobx-react';
-import { Grid, Segment, Menu }  from 'semantic-ui-react';
-// import { brand } from '../config/theme';
+import { Grid, Segment, Menu, Icon }  from 'semantic-ui-react';
+import { brand } from '../config/theme';
 
 @observer
 class CustomerView extends React.Component {
 
   render() {
+
+    const customerMenu = (
+      <Menu style={{marginBottom:0}} inverted color={brand}>
+        <Menu.Item>
+          <Icon name='chevron left'/>
+        </Menu.Item>
+      </Menu>
+    )
+
+    const invoiceMenu = (
+      <Menu inverted color={brand}>
+        <Menu.Item>
+          Customer Invoices
+        </Menu.Item>
+      </Menu>
+    )
+
     const grid = (
       <Grid stackable>
-          <Grid.Column width={6}>
+          <Grid.Column width={4} style={{paddingRight:0}}>
+            {customerMenu}
             Customer Detail
           </Grid.Column>
-          <Grid.Column width={10}>
+          <Grid.Column width={12} style={{paddingLeft:0}}>
+            {invoiceMenu}
             Customer Invoices
           </Grid.Column>
       </Grid>
