5ed93f9961689b7972284b771fd4508aff7bcfbd
Michael Wanyoike <micwanyoike@gmail.com>
2017-06-01 15:42:01 +0300

    implemented sales static summary

--- client/src/sales/summary.js
+++ client/src/sales/summary.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
+import { Icon, Statistic, Divider, Segment, Progress, Grid, Feed } from 'semantic-ui-react';
 
 class SalesSummary extends Component {
   render() {
+    const statsPanel = (
+      <Segment inverted>
+        <Statistic.Group widths='four'>
+          <Statistic inverted>
+            <Statistic.Value>8</Statistic.Value>
+            <Statistic.Label>Completed Sales</Statistic.Label>
+          </Statistic>
+          <Statistic inverted>
+            <Statistic.Value>16</Statistic.Value>
+            <Statistic.Label>New Invoices</Statistic.Label>
+          </Statistic>
+          <Statistic inverted color='teal'>
+            <Statistic.Value>9</Statistic.Value>
+            <Statistic.Label>Paid Invoices</Statistic.Label>
+          </Statistic>
+          <Statistic inverted color='red'>
+            <Statistic.Value>8</Statistic.Value>
+            <Statistic.Label>Unpaid Payments</Statistic.Label>
+          </Statistic>
+        </Statistic.Group>
+      </Segment>
+    );
+
     return (
-      <p>Sales Summary under construction</p>
+      <div>
+        <h3>Monthly Summary</h3>
+        {statsPanel}
+      </div>
     )
   }
 }
