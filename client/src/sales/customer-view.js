f65c9f5812ea93d2322f2a54d27136ca2f9f9493
Michael Wanyoike <micwanyoike@gmail.com>
2017-06-01 21:40:07 +0300

    created skeletom customer-view

---
+++ client/src/sales/customer-view.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { observer } from 'mobx-react';
+
+@observer
+class CustomerView extends React.Component {
+  render() {
+    return(
+      <div>
+        Customer View
+      </div>
+    )
+  }
+}
+
+export default Customer;
