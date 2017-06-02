81d928e488be7cba4d5e3e995fa3258cea20a2b9
Michael Wanyoike <micwanyoike@gmail.com>
2017-06-02 10:26:27 +0300

    Refactored vendors

--- client/src/inventory/vendors.js
+++ client/src/inventory/vendors.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import { Menu, Icon } from 'semantic-ui-react';
-import { NavLink, Route } from 'react-router-dom';
 import { inject } from 'mobx-react';
 import { INVENTORY_ROUTE } from '../config/routes';
-import { secondary } from '../config/theme';
-import DomainList from '../templates/domain-list';
-import DomainForm from '../templates/domain-form';
+import DomainPage from '../templates/domain-page';
 import DomainRoutes from '../templates/domain-routes';
 import DomainSchema from '../templates/domain-schema';
 
@@ -60,19 +56,7 @@ class Vendors extends React.Component {
     const schema = new DomainSchema('Vendor', this.table, this.form);
 
     return (
-      <div>
-        {/* <Menu inverted color={secondary}>
-          <NavLink className="item" activeClassName="active" exact to={routes.list}>
-            <Icon name='list'/> {schema.label}s List</NavLink>
-          <NavLink className="item" activeClassName="active" to={routes.new}>
-            <Icon name='add'/>Add {schema.label}
-          </NavLink>
-        </Menu> */}
-        <h3>{schema.label}s</h3>
-        <Route component={props => (<DomainList {...props} routes={routes} schema={schema} store={store} />)} exact path={routes.list}/>
-        <Route component={props => (<DomainForm {...props} routes={routes} schema={schema} store={store} />)} path={routes.new}/>
-        <Route component={props => (<DomainForm {...props} routes={routes} schema={schema} store={store} />)} path={routes.edit} />
-      </div>
+      <DomainPage store={store} routes={routes} schema={schema} />
     )
   }
 }
