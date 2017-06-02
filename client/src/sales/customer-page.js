5e0c43d34a44449db66cb725c6edbcbe73197dbe
Michael Wanyoike <micwanyoike@gmail.com>
2017-06-02 10:44:53 +0300

    minor cleanup for customer-page

--- client/src/sales/customer-page.js
+++ client/src/sales/customer-page.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import { Menu, Icon } from 'semantic-ui-react';
-import { NavLink, Route } from 'react-router-dom';
-import { secondary } from '../config/theme';
+import { Route } from 'react-router-dom';
 import CustomerList from './customer-list';
 import CustomerView from './customer-view';
 import DomainForm from '../templates/domain-form';
+// import DomainPage from '../templates/domain-form';
 
 export default ({store, routes, schema}) => (
   <div>
-    {/* <Menu inverted color={secondary}>
-      <NavLink className="item" activeClassName="active" exact to={routes.list}>
-        <Icon name='list'/> {schema.label}s List</NavLink>
-      <NavLink className="item" activeClassName="active" to={routes.new}>
-        <Icon name='add'/>Add {schema.label}
-      </NavLink>
-    </Menu> */}
     <h3>{schema.label}s</h3>
     <Route component={props => (<CustomerList {...props} routes={routes} schema={schema} store={store} />)} exact path={routes.list}/>
     <Route component={props => (<DomainForm {...props} routes={routes} schema={schema} store={store} />)} path={routes.new}/>
@@ -22,3 +14,18 @@ export default ({store, routes, schema}) => (
     <Route component={props => (<CustomerView {...props} routes={routes} schema={schema} store={store} />)} path={routes.view} />
   </div>
 );
+
+// class CustomerPage extends DomainPage {
+//
+//   routeComponents(store, routes, schema) {
+//     return [
+//       <Route key={routes.list} component={props => (<CustomerList {...props} routes={routes} schema={schema} store={store} />)} exact path={routes.list}/>,
+//       <Route key={routes.new} component={props => (<DomainForm {...props} routes={routes} schema={schema} store={store} />)} path={routes.new}/>,
+//       <Route key={routes.edit} component={props => (<DomainForm {...props} routes={routes} schema={schema} store={store} />)} path={routes.edit} />,
+//       <Route key={routes.view} component={props => (<CustomerView {...props} routes={routes} schema={schema} store={store} />)} path={routes.view} />
+//     ]
+//   }
+//
+// }
+//
+// export default CustomerPage;
