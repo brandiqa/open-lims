4b89ec71751c144223db3366e1c6ab2541d66188
Michael Wanyoike <micwanyoike@gmail.com>
2017-06-02 09:13:35 +0300

    disabled menus in domain pages

--- client/src/sales/customer-page.js
+++ client/src/sales/customer-page.js
@@ -8,13 +8,13 @@ import DomainForm from '../templates/domain-form';
 
 export default ({store, routes, schema}) => (
   <div>
-    <Menu inverted color={secondary}>
+    {/* <Menu inverted color={secondary}>
       <NavLink className="item" activeClassName="active" exact to={routes.list}>
         <Icon name='list'/> {schema.label}s List</NavLink>
       <NavLink className="item" activeClassName="active" to={routes.new}>
         <Icon name='add'/>Add {schema.label}
       </NavLink>
-    </Menu>
+    </Menu> */}
     <h3>{schema.label}s</h3>
     <Route component={props => (<CustomerList {...props} routes={routes} schema={schema} store={store} />)} exact path={routes.list}/>
     <Route component={props => (<DomainForm {...props} routes={routes} schema={schema} store={store} />)} path={routes.new}/>
--- client/src/templates/domain-page.js
+++ client/src/templates/domain-page.js
@@ -7,13 +7,13 @@ import DomainForm from './domain-form';
 
 export default ({store, routes, schema}) => (
   <div>
-    <Menu inverted color={secondary}>
+    {/* <Menu inverted color={secondary}>
       <NavLink className="item" activeClassName="active" exact to={routes.list}>
         <Icon name='list'/> {schema.label}s List</NavLink>
       <NavLink className="item" activeClassName="active" to={routes.new}>
         <Icon name='add'/>Add {schema.label}
       </NavLink>
-    </Menu>
+    </Menu> */}
     <h3>{schema.label}s</h3>
     <Route component={props => (<DomainList {...props} routes={routes} schema={schema} store={store} />)} exact path={routes.list}/>
     <Route component={props => (<DomainForm {...props} routes={routes} schema={schema} store={store} />)} path={routes.new}/>
