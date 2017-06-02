c5cb17f2a8a9b053d034c3d477b1e5bb449030c4
Michael Wanyoike <micwanyoike@gmail.com>
2017-06-02 10:24:05 +0300

    Refactored Domain Page to OOP

--- client/src/templates/domain-page.js
+++ client/src/templates/domain-page.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import { Menu, Icon } from 'semantic-ui-react';
-import { NavLink, Route } from 'react-router-dom';
-import { secondary } from '../config/theme';
+import { Divider, Header } from 'semantic-ui-react';
+import { Route } from 'react-router-dom';
 import DomainList from './domain-list';
 import DomainForm from './domain-form';
 
-export default ({store, routes, schema}) => (
-  <div>
-    {/* <Menu inverted color={secondary}>
-      <NavLink className="item" activeClassName="active" exact to={routes.list}>
-        <Icon name='list'/> {schema.label}s List</NavLink>
-      <NavLink className="item" activeClassName="active" to={routes.new}>
-        <Icon name='add'/>Add {schema.label}
-      </NavLink>
-    </Menu> */}
-    <h3>{schema.label}s</h3>
-    <Route component={props => (<DomainList {...props} routes={routes} schema={schema} store={store} />)} exact path={routes.list}/>
-    <Route component={props => (<DomainForm {...props} routes={routes} schema={schema} store={store} />)} path={routes.new}/>
-    <Route component={props => (<DomainForm {...props} routes={routes} schema={schema} store={store} />)} path={routes.edit} />
-  </div>
-);
+class DomainPage extends React.Component {
+
+   render() {
+     const { store, routes, schema} = this.props;
+
+     return (
+       <div>
+         <Header>{schema.label}s</Header>
+         <Divider/>
+         <Route component={props => (<DomainList {...props} routes={routes} schema={schema} store={store} />)} exact path={routes.list}/>
+         <Route component={props => (<DomainForm {...props} routes={routes} schema={schema} store={store} />)} path={routes.new}/>
+         <Route component={props => (<DomainForm {...props} routes={routes} schema={schema} store={store} />)} path={routes.edit} />
+       </div>
+     );
+   }
+}
+
+export default DomainPage;
